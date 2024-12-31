2cd580da7d3d639fd791a1a1a1091775e44598ff
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-12-31 12:51:16 +0330

    feat : add feature
    add sweet alert

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
-//                  Ajax 
+//                  Ajax
 
 // HTTP status codes : 200
 
 // Select button
-document.querySelector(".btn btn-primary btn-custom").addEventListener("click",loadDataFirstStatus)
-// Function for handling first button   
-function loadDataFirstStatus(){
-    
-    // Create object
-    const XHR = new XMLHttpRequest()
+document
+  .querySelector(".btn btn-primary btn-custom")
+  .addEventListener("click", loadDataFirstStatus);
+// Function for handling first button
+function loadDataFirstStatus() {
+  // Create object
+  const XHR = new XMLHttpRequest();
 
-    // Open a new request
-    XHR.open("GET", "./README.md")
-    
-    // Load request
-    XHR.onload = function (){
-        if (this.status === 200){
+  // Open a new request
+  XHR.open("GET", "./README.md");
 
-        }
+  // Load request
+  XHR.onload = function () {
+    if (this.status === 200) {
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>',
+      });
     }
+  };
 
-
-    // Send request
-    XHR.send()
-
-
-
+  // Send request
+  XHR.send();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // HTTP status codes
 // HTTP status codes
